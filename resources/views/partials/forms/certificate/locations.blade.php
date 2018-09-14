a9d3e9e6cee4e823d1e23468432ad5d3f0c62695
unknown <novandwianggarda@gmail.com>
2018-09-14 09:27:36 +0700

    update certificate , database file

--- resources/views/partials/forms/certificate/locations.blade.php
+++ resources/views/partials/forms/certificate/locations.blade.php
@@ -3,37 +3,47 @@
 		LOCATIONS
 	</div>
 
-	<!-- <div class="mapouter">
-		<div class="gmap_canvas">
-			<iframe width="1000" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=semarang&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-			</iframe>
-			<a href="https://www.pureblack.de"></a>
-		</div>
-		<style>.mapouter{text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}
-		</style>
-	</div> -->
-    <div>
-        <div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div id="map" style="width:100%;height:400px"></div>
-                    </div>
-                    <div class="col-md-12 item boundary_coordinates">
-                        <br>
-                        <div style="float:left;">
-                                                    <button class="btn btn-sm btn-warning" type="button" onclick="clearMap(); return false;"><i class="fa fa-trash"></i> Clear Map</button>
-                                                    <button class="btn btn-sm btn-info" type="button" onclick="makeBoundary(); return false;"><i class="fa fa-map"></i> Make Boundary</button>
-                                                    <span>&nbsp;&nbsp;<b>Area</b>: <span id="polygonArea">0</span> m<sup>2</sup></span>
-                                                </div>
-                        <div class="alert boundary_coordinates" style="">Edited by Admin</div>
-                        <div class="clearfix"></div>
-                    </div>
-                </div>
-                <input type="hidden" name="boundary_coordinates" id="boundary_coordinates">
-            </div>
-        </div>
-    </div>
+
+
+
+
+
+
+
+    <div id="map"></div>
+    <script>
+      // This example requires the Drawing library. Include the libraries=drawing
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+
+        var drawingManager = new google.maps.drawing.DrawingManager({
+          drawingMode: google.maps.drawing.OverlayType.MARKER,
+          drawingControl: true,
+          drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
+          },
+          markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+          circleOptions: {
+            fillColor: '#ffff00',
+            fillOpacity: 1,
+            strokeWeight: 5,
+            clickable: false,
+            editable: true,
+            zIndex: 1
+          }
+        });
+        drawingManager.setMap(map);
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing&callback=initMap"
+         async defer></script>
                             
 
 </accordion>
